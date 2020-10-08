d25f9adacfd239db71e2bfd6b9b464f7e933781b
Olexander Narozhniy <sasha.nevazhno2015@gmail.com>
2020-10-08 12:45:51 +0300

    Добавили ещё 1 элемент в наш линкед лист

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> some_list= new LinkedList<String>(Arrays.asList("John", "Max"));
+        LinkedList<String> some_list= new LinkedList<String>(Arrays.asList("John", "Max", "Sasha"));
             System.out.println(some_list);
 
         }
