970c78e7e4cd26ae54477dafea8018becb59c758
Olexander Narozhniy <sasha.nevazhno2015@gmail.com>
2020-10-08 17:08:08 +0300

    fixing mistakes, isn`t crucial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main extends Films{
     public static void main(String[] args) {
-        List<Films> list = new ArrayList<Films>();
+        List<Films> list = new ArrayList<>();
         //В конструкторе класса Films можно поместить метод void который уже содержит коллекцию с помещенными в него элементами!
         Films films = new Films();
         //Вызываем наш метод next()
@@ -33,24 +33,21 @@ public class Main extends Films{
         System.out.println('\n' + "Size of our list is :" + list.size());
         System.out.println("\n");
 
-        List<String> some_list = new LinkedList<String>(Arrays.asList("John", "Max", "Sasha", "Nasty"));
+        List<String> some_list = new LinkedList<>(Arrays.asList("John", "Max", "Sasha", "Nasty"));
         for (String x : some_list
         ) {
-            if (x == "John") {
+            if (x.equals("John")) {
                 System.out.println("Congratulations " + x + " You won 1 million dollars");
             } else {
-                System.out.println("Unfortunatly " + x + " You did not win 1 million");
+                System.out.println("Unfortunately " + x + " You did not win 1 million");
             }
         }
-
+        // Мы создали тренировочною коллекцию set и попробовали её вывести
         Map<Integer, Integer> some_map = new TreeMap<>();
 
         some_map.put(1, 2);
         some_map.put(2, 4);
         System.out.println(some_map.get(2));
-
-
-
 
     }
 }
